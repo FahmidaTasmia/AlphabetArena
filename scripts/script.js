02e0dc337b6d0f58a353df4a48e3628678266bcb
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-01-21 00:53:06 +0600

    Implemented utility functions: setBackgroundColorById for dynamic styling and getRandomAlphabet for generating random letters.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,13 @@
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText=alphabet;
+    setBackgroundColorById(alphabet)
+}
+
+
 function play(){
   hideElementById('home-screen');
   showElementById('play-ground');
+  continueGame();
 }
