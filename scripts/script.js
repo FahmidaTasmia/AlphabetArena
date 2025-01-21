d2df7cf80f962a40172fbabda5a3e7f2782d34e4
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-01-22 00:35:34 +0600

    Added keyboard input handler to validate player input and manage game flow dynamically.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
+function handleKeyboardButtonPress(event){
+  const playerPressed = event.key;
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  //check right key or wrong
+  if(playerPressed===expectedAlphabet){
+    console.log('you got a point');
+    
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+  }
+  else{
+    console.log('oops! you loss')
+  }
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
 function continueGame(){
     const alphabet = getRandomAlphabet();
     const currentAlphabetElement = document.getElementById('current-alphabet');
